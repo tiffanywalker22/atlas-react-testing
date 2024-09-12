1156e715f42cb21dcb335a3ce8fdd05b4d78b58d
Tiffany <tbcasper16@gmail.com>
2024-09-12 10:45:25 -0500

    updated files to have the new code from typescript project since I changed some things; all tests are passing coverart, playlistitem, song title, and volume control

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
 export default function Footer() {
-  const year = new Date().getFullYear();
-  return <div className="text-center p-8">&copy; {year} Atlas School</div>;
-}
+    const year = new Date().getFullYear();
+    return <div className="text-center p-8">&copy; {year} Atlas School</div>;
+  }
